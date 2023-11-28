014924f3fdf32ef4bb878e99ab97193cba41a1cb
David Lee <Dai180293@gmail.com>
2023-11-28 16:59:48 +0000

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        extend: {
+            fontFamily: {
+                ubuntu: ["Ubuntu", "sans-serif"],
+            },
+            colors: {
+                marineBlue: "hsl(213, 96%, 18%)",
+                purpleBlue: "hsl(243, 100%, 62%)",
+                blue: "hsl(228, 100%, 84%)",
+                lightBlue: "hsl(206, 94%, 87%)",
+                strawberryRed: "hsl(354, 84%, 57%)",
+                grey: "hsl(231, 11%, 63%)",
+                lightGrey: "hsl(229, 24%, 87%)",
+                magnolia: "hsl(217, 100%, 97%)",
+                alabaster: "hsl(231, 100%, 99%)",
+                white: "hsl(0, 0%, 100%)",
+            },
+        },
+    },
+    plugins: [],
+};
